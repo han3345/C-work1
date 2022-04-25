29a107934b92d64fa35df4b7477a7382e70e5e8f
Windows <hanbinyangwh@gmail.com>
2022-04-25 13:50:23 +0800

    init

---
+++ queue.cpp
@@ -0,0 +1,40 @@
+//线性表实现一元稀疏多项式计算
+//输入：a=5.5X^3+4X^999+.....
+//b=77777X^12344+2342342353X^34234423+.....
+//输出a+b=？
+//**仅限整数次幂！**
+#include<iostream>
+using namespace std;
+
+
+long double a[10000];//系数
+long long b[10000];//指数
+long double c[10000];//系数
+long long d[10000];//指数
+int n;
+
+void makeInput()
+{
+    // n1=rand
+    // make(a,b,n1)
+    // n2=rand
+    // make(c,d,n2)
+    // print X1,X2
+    return;
+}
+
+
+void add()
+{
+    //new queue;
+
+    return;
+}
+
+
+int main()
+{
+    makeInput();
+    add(); 
+    return 0;
+}
